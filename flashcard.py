202919dc68a17ce17b127b4395aab42a98ff1460
Kevin Chen <kchen3568@gmail.com>
2020-07-07 00:54:56 -0500

    Generates image

--- flashcard.py
+++ flashcard.py
@@ -1,18 +1,31 @@
 import xlrd
 import re
-import mechanize
+from splinter import Browser
 
 filename = "~/Downloads/ycm.xlsx"
 wb = xlrd.open_workbook(filename)
 sheet = wb.sheet_by_index(0)
+br = Browser()
+br.visit("https://www.cardmaker.net/yugioh/")
+form_ids = [
+    "name", "cardtype", "subtype", "attribute", "level", "trapmagictype", "rarity", "picture", "circulation", "set1",
+    "set2", "type", "carddescription", "atk", "def", "creator", "year", "serial"
+]
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open("https://www.cardmaker.net/yugioh/")
-br.select_form(nr=2)
-
-form_labels = [sheet.cell_value(1, i) for i in range(1, sheet.ncols)]
-for i in range(2, sheet.nrows):
-    for j in range(1, sheet.ncols):
-        pass
+for r in range(2, sheet.nrows):
+    for c in range(1, sheet.ncols):
+        form_id = form_ids[c-1]
+        form_value = sheet.cell_value(r, c)
+        if not form_value:
+            break
+        if c == 3:
+            form_value = form_value.lower()
+        elif c == 5 or c == 14 or c == 15 or c == 17:
+            form_value = str(int(form_value))
+        try:
+            br.find_by_id(form_id).first.fill(form_value)
+        except Exception:
+            br.find_by_id(form_id).first.select(form_value)
+    br.find_by_id("generate").first.click()
+    image = br.find_by_id("card")
+    print(image)
